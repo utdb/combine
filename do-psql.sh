ecf88f8ca37c15fd9eca45282019db0f85c7238f
Brend Wanders <b.wanders@utwente.nl>
2016-08-10 13:01:50 +0200

    Adds quality of life psql opener

---
+++ do-psql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+get_config() {
+    awk -F '=' "/$1/ {print \$2}" combine.local.cfg | tr -d ' '
+}
+
+HOST=$(get_config 'host')
+DB=$(get_config 'db')
+USER=$(get_config 'user')
+PASSWORD=$(get_config 'password')
+
+echo "Connecting to database '$DB' as $USER@$HOST..."
+
+PGPASSWORD="$PASSWORD" exec psql -U "$USER" -h "$HOST" -d "$DB"
